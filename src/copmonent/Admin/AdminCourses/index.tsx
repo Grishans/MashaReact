992beec80553a1cd69a868d279677fc0209e1c98
Grishans <grishak.class97@mail.ru>
2021-02-15 12:42:04 +0300

    Гриша: портфолио админка поправил

--- src/copmonent/Admin/AdminCourses/index.tsx
+++ src/copmonent/Admin/AdminCourses/index.tsx
@@ -1,88 +1,96 @@
-import { observer } from "mobx-react-lite"
-import React from "react"
-import { coursecStores } from "../../../stores/coursesStores"
-import { ICourse } from "../../../types"
-import AdminBTN from "../AdminBTN"
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { coursecStores } from "../../../stores/coursesStores";
+import { ICourse } from "../../../types";
+import AdminBTN from "../AdminBTN";
 
 const AdminCourses: React.FC = observer(
-	(): React.ReactElement => {
-		const course: ICourse[] = coursecStores.items
-		const [courseCurr, setCourseCurr] = React.useState<ICourse>()
+  (): React.ReactElement => {
+    const course: ICourse[] = coursecStores.items;
+    const [courseCurr, setCourseCurr] = React.useState<ICourse>();
 
-		const changeInput = (
-			e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-		): void => {
-			setCourseCurr((pre) => ({ ...pre, [e.target.name]: e.target.value }))
-		}
-		const saveForm = async (): Promise<void> => {
-			try {
-				await coursecStores.edit(courseCurr!)
-				setCourseCurr(undefined)
-				alert("Курсы сохранены!")
-			} catch (error) {
-				console.error(`Ошибка Курсы: ${error}`)
-			}
-		}
-		return (
-			<>
-				<div className='admin__sevices__wrap'>
-					<AdminBTN
-						Save
-						onClick={saveForm}
-						disabled={courseCurr === undefined}
-					/>
-					<div className='adminCourses__wrap'>
-						{course &&
-							course.map((item, index) => (
-								<div
-									className='admin__content__box adminCourses__box'
-									key={index}>
-									<label className='adminLabel'>{item.title}</label>{" "}
-									<button
+    const changeInput = (
+      e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ): void => {
+      setCourseCurr((pre) => ({ ...pre, [e.target.name]: e.target.value }));
+    };
+    const saveForm = async (): Promise<void> => {
+      try {
+        await coursecStores.edit(courseCurr!);
+        setCourseCurr(undefined);
+        alert("Курсы сохранены!");
+      } catch (error) {
+        console.error(`Ошибка Курсы: ${error}`);
+      }
+    };
+    return (
+      <>
+        <div className="admin__sevices__wrap">
+          <AdminBTN
+            Save
+            onClick={saveForm}
+            disabled={courseCurr === undefined}
+          />
+          <div className="adminCourses__wrap">
+            {course &&
+              course.map((item, index) => (
+                <div
+                  className="admin__content__box adminCourses__box"
+                  key={index}
+                >
+                  <label className="adminLabel">{item.title}</label>{" "}
+                  <label
+                    id="AdminEditLabel"
+                    onClick={() => setCourseCurr(item)}
+                  >
+                    Редактировать
+                  </label>
+                  {/* <button
 										onClick={() => setCourseCurr(item)}
 										disabled={courseCurr === undefined}>
 										Редактировать
-									</button>
-									<hr />
-									<label htmlFor='coursesINP1' className='adminLabel'>
-										Название курса
-									</label>
-									<input
-										id='coursesINP1'
-										type='text'
-										name='title'
-										value={
-											courseCurr && courseCurr._id === item._id
-												? courseCurr.title
-												: item.title
-										}
-										onChange={changeInput}
-										className='adminInput'
-										disabled={courseCurr && courseCurr!._id !== item._id}
-										placeholder='Введите название'
-									/>
-									<label htmlFor='coursesTA1' className='adminLabel'>
-										Описание
-									</label>
-									<textarea
-										className='adminInput'
-										id='coursesTA1'
-										name='desc'
-										value={
-											courseCurr && courseCurr._id === item._id
-												? courseCurr.desc
-												: item.desc
-										}
-										onChange={changeInput}
-										disabled={courseCurr && courseCurr!._id !== item._id}
-										placeholder='Описание'></textarea>
-								</div>
-							))}
-					</div>
-				</div>
-			</>
-		)
-	},
-)
+									</button> */}
+                  <hr />
+                  <label htmlFor="coursesINP1" className="adminLabel">
+                    Название курса
+                  </label>
+                  <input
+                    id="coursesINP1"
+                    type="text"
+                    name="title"
+                    value={
+                      courseCurr && courseCurr._id === item._id
+                        ? courseCurr.title
+                        : item.title
+                    }
+                    onChange={changeInput}
+                    className="adminInput"
+                    disabled={courseCurr && courseCurr!._id !== item._id}
+                    placeholder="Введите название"
+                  />
+                  <label htmlFor="coursesTA1" className="adminLabel">
+                    Описание
+                  </label>
+                  <textarea
+                    className="adminInput"
+                    id="coursesTA1"
+                    name="desc"
+                    value={
+                      courseCurr && courseCurr._id === item._id
+                        ? courseCurr.desc
+                        : item.desc
+                    }
+                    onChange={changeInput}
+                    disabled={courseCurr && courseCurr!._id !== item._id}
+                    placeholder="Описание"
+                  ></textarea>
+                </div>
+              ))}
+          </div>
+        </div>
+      </>
+    );
+  }
+);
 
-export default AdminCourses
+export default AdminCourses;
--- src/copmonent/Admin/AdminSerices/indes.tsx
+++ src/copmonent/Admin/AdminSerices/indes.tsx
@@ -1,207 +1,218 @@
-import classNames from "classnames"
-import { observer } from "mobx-react-lite"
-import React from "react"
-import { servicesStores } from "../../../stores/servicesStores"
-import { IService } from "../../../types"
-import { attach } from "../../../utils/attachFiles"
-import AdminBTN from "../AdminBTN"
+import classNames from "classnames";
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { servicesStores } from "../../../stores/servicesStores";
+import { IService } from "../../../types";
+import { attach } from "../../../utils/attachFiles";
+import AdminBTN from "../AdminBTN";
 
 const AdminServices: React.FC = observer(
-	(): React.ReactElement => {
-		const [addBox, setAddBox] = React.useState<boolean>(false)
-		const service: IService[] = servicesStores.items
-		const [image, setImage] = React.useState<Blob | any>()
-		const [serCurrent, setSerCurrent] = React.useState<IService | undefined>()
+  (): React.ReactElement => {
+    const [addBox, setAddBox] = React.useState<boolean>(false);
+    const service: IService[] = servicesStores.items;
+    const [image, setImage] = React.useState<Blob | any>();
+    const [serCurrent, setSerCurrent] = React.useState<IService | undefined>();
 
-		const changeInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
-			setSerCurrent((pre) => ({ ...pre, [e.target.name]: e.target.value }))
-		}
+    const changeInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
+      setSerCurrent((pre) => ({ ...pre, [e.target.name]: e.target.value }));
+    };
 
-		const addImage = (e: React.ChangeEvent<HTMLInputElement>): void => {
-			const file = e.target.files
-			setImage(file![0])
-		}
-		const saveService = async (): Promise<void> => {
-			try {
-				const pic = await attach(image)
-				if (!serCurrent!._id || serCurrent!._id === undefined) {
-					const obj: IService = {
-						...serCurrent,
-						photo: pic,
-					}
-					await servicesStores.create(obj)
-					alert("Услуга сохранена!")
-				} else {
-					await servicesStores.edit(serCurrent!)
-					alert("Услуга обновленна!")
-				}
-				setAddBox(false)
-				setSerCurrent(undefined)
-				setImage(undefined)
-			} catch (error) {
-				console.error(`Ошибка Сервисы(Сохранение): ${error}`)
-			}
-		}
+    const addImage = (e: React.ChangeEvent<HTMLInputElement>): void => {
+      const file = e.target.files;
+      setImage(file![0]);
+    };
+    const saveService = async (): Promise<void> => {
+      try {
+        const pic = await attach(image);
+        if (!serCurrent!._id || serCurrent!._id === undefined) {
+          const obj: IService = {
+            ...serCurrent,
+            photo: pic,
+          };
+          await servicesStores.create(obj);
+          alert("Услуга сохранена!");
+        } else {
+          await servicesStores.edit(serCurrent!);
+          alert("Услуга обновленна!");
+        }
+        setAddBox(false);
+        setSerCurrent(undefined);
+        setImage(undefined);
+      } catch (error) {
+        console.error(`Ошибка Сервисы(Сохранение): ${error}`);
+      }
+    };
 
-		const showBox = React.useCallback(() => {
-			setAddBox((prev) => !prev)
-		}, [])
+    const showBox = React.useCallback(() => {
+      setAddBox((prev) => !prev);
+    }, []);
 
-		React.useEffect(() => {
-			const addBTN = document.querySelector<HTMLButtonElement>("#adminADD")
-			addBTN!.onclick = showBox
-		})
-		return (
-			<>
-				<div className='admin__sevices__wrap'>
-					<div className='admin__services__btn'>
-						<AdminBTN Add />
-						<AdminBTN Save onClick={saveService} />
-					</div>
-					<div className='admin__service__content__wrap'>
-						{addBox && (
-							<div className='admin__content__box admin__service__comtent__box'>
-								<div className='ascb__title'>
-									<label>
-										{serCurrent &&
-										serCurrent!.title &&
-										serCurrent!.title!.length > 0
-											? serCurrent!.title
-											: "Новая услуга"}
-									</label>
-									<AdminBTN
-										Cross
-										onClick={(): void => {
-											setAddBox(false)
-										}}
-									/>
-								</div>
-								<hr />
-								<div className='ascb__inputs'>
-									<div className='ascb__inputs__insert'>
-										<label htmlFor='adminInput' className='adminLabel'>
-											Название услуги
-										</label>
-										<input
-											id='adminInput'
-											name='title'
-											value={serCurrent && serCurrent!.title}
-											onChange={changeInput}
-											type='text'
-											className='adminInput'
-											placeholder='Введите название'
-										/>
-										<label htmlFor='adminInput' className='adminLabel'>
-											Затраты времени
-										</label>
-										<input
-											id='adminInput'
-											type='text'
-											name='workTime'
-											value={serCurrent && serCurrent!.workTime}
-											onChange={changeInput}
-											className='adminInput'
-											placeholder='Введите время затрат'
-										/>
-									</div>
-									<div
-										className={classNames("ascb__inputs__addNew", {
-											"ascb__inputs__addNew-prev": image,
-										})}>
-										<input id='inpFile' type='file' onChange={addImage} />
-										<label htmlFor='inpFile'>
-											<img
-												src={
-													image
-														? URL.createObjectURL(image)
-														: "/img/admin_camera.png"
-												}
-												alt=''
-											/>
-										</label>
-									</div>
-								</div>
-							</div>
-						)}
-						{service &&
-							service.length > 0 &&
-							service.map((item, index) => (
-								<div
-									className='admin__content__box admin__service__comtent__box'
-									key={index + item.title!}>
-									<div className='ascb__title'>
-										<label>{item.title}</label>
-										<button onClick={() => setSerCurrent(item)}>
-											Редактировать
-										</button>
-										<AdminBTN
-											Cross
-											onClick={(): void => {
-												if (window.confirm("Вы точно хотите удалить услугу?")) {
-													servicesStores.delete(item._id!)
-												}
-											}}
-										/>
-									</div>
-									<hr />
-									<div className='ascb__inputs'>
-										<div className='ascb__inputs__insert'>
-											<label htmlFor='adminInput' className='adminLabel'>
-												Название услуги
-											</label>
-											<input
-												id='adminInput'
-												type='text'
-												name='title'
-												value={
-													serCurrent && serCurrent.title !== ""
-														? serCurrent.title
-														: item.title
-												}
-												onChange={changeInput}
-												className='adminInput'
-												placeholder='Введите название'
-												disabled={serCurrent && serCurrent!._id !== item._id}
-											/>
-											<label htmlFor='adminInput' className='adminLabel'>
-												Затраты времени
-											</label>
-											<input
-												id='adminInput'
-												type='text'
-												name='workTime'
-												value={
-													serCurrent && serCurrent.workTime !== ""
-														? serCurrent.workTime
-														: item.workTime
-												}
-												onChange={changeInput}
-												className='adminInput'
-												placeholder='Введите время затрат'
-												disabled={serCurrent && serCurrent!._id !== item._id}
-											/>
-										</div>
-										<div className='ascb__inputs__img'>
-											<img
-												src={
-													image
-														? URL.createObjectURL(image)
-														: `${process.env.REACT_APP_LINK}${item.photo}`
-												}
-												alt=''
-											/>
-											<div className='ascb__inputs__addImg'>
-												<label>Добавить Фотографию</label>
-											</div>
-										</div>
-									</div>
-								</div>
-							))}
-					</div>
-				</div>
-			</>
-		)
-	},
-)
+    React.useEffect(() => {
+      const addBTN = document.querySelector<HTMLButtonElement>("#adminADD");
+      addBTN!.onclick = showBox;
+    });
+    return (
+      <>
+        <div className="admin__sevices__wrap">
+          <div className="admin__services__btn">
+            <AdminBTN Add />
+            <AdminBTN Save onClick={saveService} />
+          </div>
+          <div className="admin__service__content__wrap">
+            {addBox && (
+              <div className="admin__content__box admin__service__comtent__box">
+                <div className="ascb__title">
+                  <label>
+                    {serCurrent &&
+                    serCurrent!.title &&
+                    serCurrent!.title!.length > 0
+                      ? serCurrent!.title
+                      : "Новая услуга"}
+                  </label>
+                  <AdminBTN
+                    Cross
+                    onClick={(): void => {
+                      setAddBox(false);
+                    }}
+                  />
+                </div>
+                <hr />
+                <div className="ascb__inputs">
+                  <div className="ascb__inputs__insert">
+                    <label htmlFor="adminInput" className="adminLabel">
+                      Название услуги
+                    </label>
+                    <input
+                      id="adminInput"
+                      name="title"
+                      value={serCurrent && serCurrent!.title}
+                      onChange={changeInput}
+                      type="text"
+                      className="adminInput"
+                      placeholder="Введите название"
+                    />
+                    <label htmlFor="adminInput" className="adminLabel">
+                      Затраты времени
+                    </label>
+                    <input
+                      id="adminInput"
+                      type="text"
+                      name="workTime"
+                      value={serCurrent && serCurrent!.workTime}
+                      onChange={changeInput}
+                      className="adminInput"
+                      placeholder="Введите время затрат"
+                    />
+                  </div>
+                  <div
+                    className={classNames("ascb__inputs__addNew", {
+                      "ascb__inputs__addNew-prev": image,
+                    })}
+                  >
+                    <input id="inpFile" type="file" onChange={addImage} />
+                    <label htmlFor="inpFile">
+                      <img
+                        src={
+                          image
+                            ? URL.createObjectURL(image)
+                            : "/img/admin_camera.png"
+                        }
+                        alt=""
+                      />
+                    </label>
+                  </div>
+                </div>
+              </div>
+            )}
+            {service &&
+              service.length > 0 &&
+              service.map((item, index) => (
+                <div
+                  className="admin__content__box admin__service__comtent__box"
+                  key={index + item.title!}
+                >
+                  <div className="ascb__title">
+                    <label>{item.title}</label>
+                    <label
+                      id="AdminEditLabel"
+                      onClick={() => setSerCurrent(item)}
+                    >
+                      Редактировать
+                    </label>
+                    {/* <button onClick={() => setSerCurrent(item)}>
+                      Редактировать
+                    </button> */}
+                    <AdminBTN
+                      Cross
+                      onClick={(): void => {
+                        if (window.confirm("Вы точно хотите удалить услугу?")) {
+                          servicesStores.delete(item._id!);
+                        }
+                      }}
+                    />
+                  </div>
+                  <hr />
+                  <div className="ascb__inputs">
+                    <div className="ascb__inputs__insert">
+                      <label htmlFor="adminInput" className="adminLabel">
+                        Название услуги
+                      </label>
+                      <input
+                        id="adminInput"
+                        type="text"
+                        name="title"
+                        value={
+                          serCurrent && serCurrent.title !== ""
+                            ? serCurrent.title
+                            : item.title
+                        }
+                        onChange={changeInput}
+                        className="adminInput"
+                        placeholder="Введите название"
+                        disabled={serCurrent && serCurrent!._id !== item._id}
+                      />
+                      <label htmlFor="adminInput" className="adminLabel">
+                        Затраты времени
+                      </label>
+                      <input
+                        id="adminInput"
+                        type="text"
+                        name="workTime"
+                        value={
+                          serCurrent && serCurrent.workTime !== ""
+                            ? serCurrent.workTime
+                            : item.workTime
+                        }
+                        onChange={changeInput}
+                        className="adminInput"
+                        placeholder="Введите время затрат"
+                        disabled={serCurrent && serCurrent!._id !== item._id}
+                      />
+                    </div>
+                    <div className="ascb__inputs__img">
+                      <img
+                        src={
+                          image
+                            ? URL.createObjectURL(image)
+                            : `${process.env.REACT_APP_LINK}${item.photo}`
+                        }
+                        alt=""
+                      />
+                      <div className="ascb__inputs__addImg">
+                        <label htmlFor="adminSevicesImgAdd">
+                          Добавить Фотографию
+                        </label>
+                        <input type="file" id="adminSevicesImgAdd" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </>
+    );
+  }
+);
 
-export default AdminServices
+export default AdminServices;
